c3aced6f29119fa5705f23a84addf0a999addea9
karunabpharm <37515719+karunabpharm@users.noreply.github.com>
2018-03-23 14:08:56 +0000

    new statement added

--- SeleniumAutomation/src/testNG/YahooMail.java
+++ SeleniumAutomation/src/testNG/YahooMail.java
@@ -5,6 +5,7 @@ public class YahooMail {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("heloooo");
+		System.out.println("this is Git class");
 	}
 
 }
